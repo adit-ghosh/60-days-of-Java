9f7c3f96d6531dc77752b4ef8ea063ddcfcb48b5
Adit Ghosh <aditghosh5112005@gmail.com>
2025-08-17 01:12:31 +0530

    Setting Up Java Development Environment

---
+++ Day-5/MagicPotionCalculator/src/MagicalPotionCalculator.java
@@ -0,0 +1,11 @@
+public class MagicPotionCalculator {
+    public static void main(String[] args) {
+        int magicalEssence = 5;
+        int dragonScale = 3;
+        int unicornHair = 2;
+        int potionPower = magicalEssence * dragonScale + unicornHair;
+
+        System.out.println("The power of the potion is "+potionPower);
+
+    }
+}
